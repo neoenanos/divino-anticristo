36f2d2c4013d38b955a77c33e8362b26bddcb427
Hero Protagonist <heroprotagonist32@gmail.com>
2026-04-22 22:49:13 -0400

    feat: add buscador

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,13 +3,14 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 import netlify from '@astrojs/netlify';
+import pagefind from 'astro-pagefind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://divinoanticristo.cl',
   base: '/',
 
-  integrations: [sitemap()],
+  integrations: [sitemap(), pagefind()],
 
   markdown: {
     shikiConfig: {
